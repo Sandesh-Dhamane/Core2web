032cf615a194e8d9ccec66b58431e2bb373a4c37
Sandesh-Dhamane <dhamanesandesh2003@gmail.com>
2023-11-07 19:29:38 +0530

    Assignment 5 nested for loop

---
+++ Assignment 5 nested for loop/program4.py
@@ -0,0 +1,13 @@
+'''
+1 2 3 4
+1 2 3 4
+1 2 3 4
+1 2 3 4
+'''
+for i in range (4):
+    num=1
+    for j in range(4):
+        print(num,end=" ")
+        num=num+1
+    print()
+
---
+++ Assignment 5 nested for loop/program9.py
@@ -0,0 +1,15 @@
+'''
+1 3 5
+3 5 7
+5 7 9
+'''
+
+num=1
+for i in range(3):
+    #num=i+1
+    for j in range(3):
+        print(num,end=" ")
+        
+        num=num+2
+    num=num-4
+    print()
