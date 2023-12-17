668959294ed8611e9f90154e12dd6f7d405cb749
Sandesh-Dhamane <dhamanesandesh2003@gmail.com>
2023-12-17 19:08:43 +0530

    assignment 3

---
+++ Assignment 3 for loop/program1.py
@@ -0,0 +1,8 @@
+
+'''
+Write a program to print the first 10 number
+
+'''
+for i in range(1,11):
+    print (i)
+
---
+++ Assignment 3 for loop/program4.py
@@ -0,0 +1,7 @@
+'''
+Write a program to print even numbers 1-100
+
+'''
+for i in range (1,101):
+    if(i%2==0):
+        print(i)
